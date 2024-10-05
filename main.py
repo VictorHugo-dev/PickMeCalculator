0575656f697a74cb118f04b6150b9a16828b326d
VictorHugo-dev <139052225+VictorHugo-dev@users.noreply.github.com>
2024-10-05 10:36:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,30 @@
+import pydle
 import random
 import asyncio
-from twitchio.ext import commands
 
-class Bot(commands.Bot):
+class TwitchBot(pydle.Client):
+    async def on_connect(self):
+        # Join the channel
+        await self.join('oMeiaUm')
+        print('Connected to the channel!')
 
-    def __init__(self):
-        # Set the token and channel you want the bot to join
-        super().__init__(token='ddch1kfwybbh11ogadd0n88o3xsytk', prefix='!', initial_channels=['oMeiaUm', 'VictorHugor'])
-
-    async def event_ready(self):
-        print(f'Logged in as | {self.nick}')
-        print(f'User id is | {self.user_id}')
-
-    # Define the !pickme command
-    @commands.command(name='pickme')
-    async def pickme(self, ctx):
-        message = ctx.message.content
-        sender = ctx.author.name
-
-        # Check if the command contains a mentioned user
-        if "@" in message:
-            # Extract the mentioned user from the message
-            target_user = message.split('@')[1].strip()
-            
-            # Special case: if sender or target user is GeorgeLucas95
-            if sender == "GeorgeLucas95" or target_user == "GeorgeLucas95":
-                pickme_percentage = 1052
+    async def on_message(self, target, source, message):
+        print(f'Received message from {source}: {message}')
+        
+        if message.startswith('!pickme'):
+            if "@" in message:
+                target_user = message.split('@')[1].strip()
+                pickme_percentage = 1052 if source == "GeorgeLucas95" or target_user == "GeorgeLucas95" else random.randint(0, 100)
+                await self.message(target, f'@{target_user}, sua porcentagem de pick me é: {pickme_percentage}%')
             else:
-                pickme_percentage = random.randint(0, 100)
-                
-            await ctx.send(f'@{target_user}, sua porcentagem de pick me é: {pickme_percentage}%')
-        else:
-            # Special case: if sender is GeorgeLucas95
-            if sender == "GeorgeLucas95":
-                pickme_percentage = 1052
-            else:
-                pickme_percentage = random.randint(0, 100)
-                
-            await ctx.send(f'@{sender}, sua porcentagem de pick me é: {pickme_percentage}%')
+                pickme_percentage = 1052 if source == "GeorgeLucas95" else random.randint(0, 100)
+                await self.message(target, f'@{source}, sua porcentagem de pick me é: {pickme_percentage}%')
 
-# Define a function to start the bot with asyncio.run()
 async def main():
-    bot = Bot()
-    await bot.start()
+    # Replace with your bot token
+    bot = TwitchBot(nickname='PickMeCalculator', realname='Pick Me Calculator', username='ddch1kfwybbh11ogadd0n88o3xsytk')
+    await bot.connect('irc.chat.twitch.tv', 6667)
+    await bot.handle_forever()
 
 if __name__ == "__main__":
     asyncio.run(main())
